3d05fdc3fa764fa552fac88a0aae552f95e0a494
Marek Hulan <mhulan@redhat.com>
2015-03-12 23:08:55 +0200

    Fixes #9726 - change foreman API authentication
    Chef signature authentication was refactored similarly to other
    authentication methods in smart proxy. For ForemanApi we use
    exclusively this method.
    
    Also logging of the authentication process and ForemanApi results
    was greatly improved.

--- lib/smart_proxy_chef_plugin/authentication.rb
+++ lib/smart_proxy_chef_plugin/authentication.rb
@@ -4,45 +4,54 @@ require 'base64'
 require 'openssl'
 
 module ChefPlugin
-  class Authentication
-    include ConnectionHelper
+  module Authentication
+    def authenticate_with_chef_signature
+      helpers ConnectionHelper, InstanceMethods, ::Proxy::Helpers
 
-    def verify_signature_request(client_name, signature,body)
-      #We need to retrieve client public key to verify signature
-      begin
-        client = get_connection.clients.fetch(client_name)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError,
-        Errno::ECONNREFUSED, OpenSSL::SSL::SSLError => e
-        raise Proxy::Error::Unauthorized, "Failed to authenticate node: "+e.message
+      before do
+        authenticate_chef_signature(request)
       end
-
-      raise Proxy::Error::Unauthorized, "Could not find client with name #{client_name}" if client.nil?
-      public_key = OpenSSL::PKey::RSA.new(client.public_key)
-
-      #signature is base64 encoded
-      decoded_signature = Base64.decode64(signature)
-      hash_body = Digest::SHA256.hexdigest(body)
-      public_key.verify(OpenSSL::Digest::SHA256.new, decoded_signature, hash_body)
     end
 
-    def authenticated(request, &block)
-      content     = request.env["rack.input"].read
+    module InstanceMethods
+      def verify_signature_request(client_name, signature,body)
+        #We need to retrieve client public key to verify signature
+        begin
+          client = get_connection.clients.fetch(client_name)
+        rescue StandardError => e
+          log_halt 401, "Failed to authenticate node: " + e.message + "\n#{e.backtrace.join("\n")}"
+        end
 
-      auth = true
-      if ChefPlugin::Plugin.settings.chef_authenticate_nodes
-        client_name = request.env['HTTP_X_FOREMAN_CLIENT']
-        signature   = request.env['HTTP_X_FOREMAN_SIGNATURE']
+        log_halt 401, "Could not find client with name #{client_name}" if client.nil?
+        public_key = OpenSSL::PKey::RSA.new(client.public_key)
 
-        raise Proxy::Error::Unauthorized, "Failed to authenticate node #{client_name}. Missing some headers" if client_name.nil? or signature.nil?
-        auth = verify_signature_request(client_name, signature, content)
+        #signature is base64 encoded
+        decoded_signature = Base64.decode64(signature)
+        hash_body = Digest::SHA256.hexdigest(body)
+        public_key.verify(OpenSSL::Digest::SHA256.new, decoded_signature, hash_body)
       end
 
-      if auth
-        raise Proxy::Error::BadRequest, "Body is empty for node #{client_name}" if content.nil?
-        block.call(content)
-      else
-        raise Proxy::Error::Unauthorized, "Failed to authenticate node #{client_name}"
+      def authenticate_chef_signature(request)
+        logger.debug('starting chef signature authentication')
+        content     = request.env["rack.input"].read
+
+        auth = true
+        if ChefPlugin::Plugin.settings.chef_authenticate_nodes
+          client_name = request.env['HTTP_X_FOREMAN_CLIENT']
+          logger.debug("header HTTP_X_FOREMAN_CLIENT: #{client_name}")
+          signature   = request.env['HTTP_X_FOREMAN_SIGNATURE']
+
+          log_halt 401, "Failed to authenticate node #{client_name}. Missing some headers" if client_name.nil? or signature.nil?
+          auth = verify_signature_request(client_name, signature, content)
+        end
+
+        if auth
+          log_halt 406, "Body is empty for node #{client_name}" if content.nil?
+          logger.debug("#{client_name} authenticated successfully")
+          return true
+        else
+          log_halt 401, "Failed to authenticate node #{client_name}"
+        end
       end
     end
   end
